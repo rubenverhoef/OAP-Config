85b526ecd4ea8a5a295f9ff0e14773c026bba75e
rubenverhoef <ruben_verhoef@hotmail.com>
2019-04-02 20:06:25 +0200

    add check if running as sudo remove sudo commands because script is running as sudo

--- oap-config.sh
+++ oap-config.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please use sudo."
+  exit
+fi
+
 # Activate SSH root
 function remove_ssh_message() {
-    sudo rm -f /etc/profile.d/sshpwd.sh
-    sudo rm -f /etc/xdg/lxsession/LXDE-pi/sshpwd.sh
+    rm -f /etc/profile.d/sshpwd.sh
+    rm -f /etc/xdg/lxsession/LXDE-pi/sshpwd.sh
 }
 
 # RPI init (set country, language and timezone)
 function rpi_init() {
     # Remove piwiz
-    sudo apt-get remove piwiz -y
+    apt-get remove piwiz -y
     # WiFi
-    sudo wpa_cli -i wlan0 set country NL
-    sudo iw reg set NL
-    sudo wpa_cli -i wlan0 save_config
+    wpa_cli -i wlan0 set country NL
+    iw reg set NL
+    wpa_cli -i wlan0 save_config
 
     # Timezone
-    sudo rm /etc/timezone
-    sudo sh -c "echo 'Europe/Amsterdam' >> /etc/timezone"
-    sudo rm /etc/localtime
-    sudo dpkg-reconfigure --frontend noninteractive tzdata
+    rm /etc/timezone
+    sh -c "echo 'Europe/Amsterdam' >> /etc/timezone"
+    rm /etc/localtime
+    dpkg-reconfigure --frontend noninteractive tzdata
 
     # Locale
-    sudo sed -i 's/^# nl_NL.UTF-8 UTF-8/nl_NL.UTF-8 UTF-8/' /etc/locale.gen
-    sudo locale-gen
-    sudo LC_ALL=nl_NL.UTF-8 LANG=nl_NL.UTF-8 LANGUAGE=nl_NL.UTF-8 update-locale LC_ALL=nl_NL.UTF-8 LANG=nl_NL.UTF-8 LANGUAGE=nl_NL.UTF-8
+    sed -i 's/^# nl_NL.UTF-8 UTF-8/nl_NL.UTF-8 UTF-8/' /etc/locale.gen
+    locale-gen
+    LC_ALL=nl_NL.UTF-8 LANG=nl_NL.UTF-8 LANGUAGE=nl_NL.UTF-8 update-locale LC_ALL=nl_NL.UTF-8 LANG=nl_NL.UTF-8 LANGUAGE=nl_NL.UTF-8
 }
 
 # Set Wallpaper
 function set_wallpaper() {
     install -m 644 /boot/OAP-Config/wallpaper.png                      "/home/pi"
     install -m 644 /boot/OAP-Config/wallpaper.png                      "/usr/share/plymouth/themes/pix/splash.png"
-    sudo sed -i "s/wallpaper=.*/wallpaper=\/home\/pi\/wallpaper.png/g" /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
+    sed -i "s/wallpaper=.*/wallpaper=\/home\/pi\/wallpaper.png/g" /home/pi/.config/pcmanfm/LXDE-pi/desktop-items-0.conf
 }
 
 # Remove unwanted OAP apps
@@ -58,80 +63,77 @@ function install_rearcam() {
 function install_services() {
     install -m 644 /boot/OAP-Config/services/user_startup.service                "/etc/systemd/system/"
     install -m 644 /boot/OAP-Config/services/gpio_shutdown.service               "/etc/systemd/system/"
-    # install -m 644 /boot/OAP-Config/services/custombrightness.service            "/etc/systemd/system/"
 
     install -d "/opt/OAP"
     install -m 755 /boot/OAP-Config/scripts/service_user_startup.sh             "/opt/OAP/"
     install -m 755 /boot/OAP-Config/scripts/service_gpio_shutdown.sh            "/opt/OAP/"
-    # install -m 755 /boot/OAP-Config/scripts/service_custombrightness.sh         "/opt/OAP/"
 }
 
 # Activate services
 function activate_services() {
     systemctl enable user_startup.service
     systemctl enable gpio_shutdown.service
-    # systemctl enable custombrightness.service
 }
 
 # Shutdown functions
 function relay_config() {
-    sudo sed -i '/./,/^$/!d' /boot/config.txt
-    sudo sed -i 's/^dtoverlay=gpio-poweroff.*//' /boot/config.txt
-    sudo sed -i 's/^# GPIO triggerd poweroff.*//' /boot/config.txt
-    sudo sed -i '/./,/^$/!d' /boot/config.txt
-    sudo sh -c "echo '' >> /boot/config.txt"
-    sudo sh -c "echo '# GPIO triggerd poweroff' >> /boot/config.txt"
-    sudo sh -c "echo 'dtoverlay=gpio-poweroff,gpiopin=5,active_low="y"' >> /boot/config.txt"
+    sed -i '/./,/^$/!d' /boot/config.txt
+    sed -i 's/^dtoverlay=gpio-poweroff.*//' /boot/config.txt
+    sed -i 's/^# GPIO triggerd poweroff.*//' /boot/config.txt
+    sed -i '/./,/^$/!d' /boot/config.txt
+    sh -c "echo '' >> /boot/config.txt"
+    sh -c "echo '# GPIO triggerd poweroff' >> /boot/config.txt"
+    sh -c "echo 'dtoverlay=gpio-poweroff,gpiopin=5,active_low="y"' >> /boot/config.txt"
 }
 
 # Power settings
 function power_config() {
-    sudo sed -i '/./,/^$/!d' /boot/config.txt
-    sudo sed -i 's/^max_usb_current.*//' /boot/config.txt
-    sudo sed -i 's/^# Custom power settings.*//' /boot/config.txt
-    sudo sed -i '/./,/^$/!d' /boot/config.txt
-    sudo sh -c "echo '' >> /boot/config.txt"
-    sudo sh -c "echo '# Custom power settings' >> /boot/config.txt"
-    sudo sh -c "echo 'max_usb_current=1' >> /boot/config.txt"
+    sed -i '/./,/^$/!d' /boot/config.txt
+    sed -i 's/^max_usb_current.*//' /boot/config.txt
+    sed -i 's/^# Custom power settings.*//' /boot/config.txt
+    sed -i '/./,/^$/!d' /boot/config.txt
+    sh -c "echo '' >> /boot/config.txt"
+    sh -c "echo '# Custom power settings' >> /boot/config.txt"
+    sh -c "echo 'max_usb_current=1' >> /boot/config.txt"
 }
 
 # uGreen DAB I2S
 function activate_dab_i2s() {
-    sudo sed -i 's/^dtparam=spi=on.*//' /boot/config.txt
-    sudo sed -i 's/^dtparam=i2s=on.*//' /boot/config.txt
-    sudo sed -i 's/^dtoverlay=audiosense-pi.*//' /boot/config.txt
-    sudo sh -c "echo 'dtparam=spi=on' >> /boot/config.txt"
-    sudo sh -c "echo 'dtparam=i2s=on' >> /boot/config.txt"
-    sudo sh -c "echo 'dtoverlay=audiosense-pi' >> /boot/config.txt"
+    sed -i 's/^dtparam=spi=on.*//' /boot/config.txt
+    sed -i 's/^dtparam=i2s=on.*//' /boot/config.txt
+    sed -i 's/^dtoverlay=audiosense-pi.*//' /boot/config.txt
+    sh -c "echo 'dtparam=spi=on' >> /boot/config.txt"
+    sh -c "echo 'dtparam=i2s=on' >> /boot/config.txt"
+    sh -c "echo 'dtoverlay=audiosense-pi' >> /boot/config.txt"
 }
 
 # RTC functions
 function activate_rtc() {
     activate_i2c
-    sudo sed -i '/./,/^$/!d' /boot/config.txt
-    sudo sed -i 's/^# RTC Setup.*//' /boot/config.txt
-    sudo sed -i '/dtoverlay=i2c-rtc/d' /boot/config.txt
-    sudo sed -i '/./,/^$/!d' /boot/config.txt
-    sudo sh -c "echo '' >> /boot/config.txt"
-    sudo sh -c "echo '# RTC Setup' >> /boot/config.txt"
-    sudo sh -c "echo 'dtoverlay=i2c-rtc,ds3231' >> /boot/config.txt"
-    # sudo systemctl enable hwclock-load.service >/dev/null 2>&1
-    # sudo systemctl daemon-reload
-	# sudo timedatectl set-timezone "$(cat /etc/timezone)"
+    sed -i '/./,/^$/!d' /boot/config.txt
+    sed -i 's/^# RTC Setup.*//' /boot/config.txt
+    sed -i '/dtoverlay=i2c-rtc/d' /boot/config.txt
+    sed -i '/./,/^$/!d' /boot/config.txt
+    sh -c "echo '' >> /boot/config.txt"
+    sh -c "echo '# RTC Setup' >> /boot/config.txt"
+    sh -c "echo 'dtoverlay=i2c-rtc,ds3231' >> /boot/config.txt"
+    # systemctl enable hwclock-load.service >/dev/null 2>&1
+    # systemctl daemon-reload
+	# timedatectl set-timezone "$(cat /etc/timezone)"
 }
 
 function activate_i2c() {
-    sudo sed -i '/./,/^$/!d' /boot/config.txt
-    sudo sed -i '/dtoverlay=i2c_arm=/d' /boot/config.txt
-    sudo sed -i 's/^# I2C Bus.*//' /boot/config.txt
-    sudo sed -i '/./,/^$/!d' /boot/config.txt
-    sudo sh -c "echo '' >> /boot/config.txt"
-    sudo sh -c "echo '# I2C Bus' >> /boot/config.txt"
-    sudo sh -c "echo 'dtoverlay=i2c_arm=on' >> /boot/config.txt"
+    sed -i '/./,/^$/!d' /boot/config.txt
+    sed -i '/dtoverlay=i2c_arm=/d' /boot/config.txt
+    sed -i 's/^# I2C Bus.*//' /boot/config.txt
+    sed -i '/./,/^$/!d' /boot/config.txt
+    sh -c "echo '' >> /boot/config.txt"
+    sh -c "echo '# I2C Bus' >> /boot/config.txt"
+    sh -c "echo 'dtoverlay=i2c_arm=on' >> /boot/config.txt"
 }
 
 function set_permissions() {
-    sudo chown -R pi:pi /home/pi
+    chown -R pi:pi /home/pi
 }
 
 killall autoapp
