24dd1293eca80bf89512a4111bff61e0de1dbcec
Ruben Verhoef <ruben_verhoef@hotmail.com>
2019-05-01 20:45:28 +0200

    [Audio] Fix #32
    - AA_VOICE and AA_ASSISTANT will be directly outputed to USB soundcard
    - AA_MUSIC, DAB and AUX will output to Faded sink which is default and will be set by OAP

--- scripts/service_OAP_startup.sh
+++ scripts/service_OAP_startup.sh
@@ -25,22 +25,39 @@ sudo /opt/OAP/radio_cli -b D -o 1
 runuser -l pi -c "pactl load-module module-null-sink sink_name=Faded sink_properties=device.description='Faded_Sink'"
 # Redirect Virtual sink to audio output sink
 runuser -l pi -c "pactl load-module module-loopback source=Faded.monitor sink=$SINK"
+# Make Faded default
+runuser -l pi -c "pacmd set-default-sink Faded"
 # Redirect DAB audio to faded output
 DAB_MOD=$(runuser -l pi -c "pactl load-module module-loopback source=$DAB sink=Faded")
 
 # Some variables
 AUX_STATE=0
 FADE_STATE=0
+SET_SINK=0
 AUX_MOD=0
 
 # Main loop for: (checking inputs, )
 for (( ; ; ))
 do
     # Shutdown trigger
-    IGNITION_GPIO=`gpio -g read 13`
     AA_RUNNING=$(runuser -l pi -c "pacmd list-sink-inputs" | awk 'BEGIN { ORS=" " } /index:/ {printf "\r\n%s ", $2;} /state:/ {print $2} /channel map:/ {print $3} /application.process.binary =/ {print $3};' | grep "autoapp" | awk '{ print $1 }')
-    if [ $IGNITION_GPIO -ne 0 ] && [ -z "$AA_RUNNING" ]; then
-        sudo shutdown -h now
+    AA_ASSISTANT=$(runuser -l pi -c "pacmd list-source-outputs" | awk 'BEGIN { ORS=" " } /index:/ {printf "/r/n%s ", $2;} /channel map:/ {print $3} /application.process.binary =/ {print $3};' | grep "mono" | grep "autoapp" | awk '{ print $1 }')
+    AA_VOICE=$(runuser -l pi -c "pacmd list-sink-inputs" | awk 'BEGIN { ORS=" " } /index:/ {printf "\r\n%s ", $2;} /state:/ {print $2} /channel map:/ {print $3} /application.process.binary =/ {print $3};' | grep "autoapp" | grep "RUNNING" | grep "mono"  | awk '{ print $1 }')
+
+    if [ -z "$AA_RUNNING" ]; then
+        SET_SINK=0
+        IGNITION_GPIO=`gpio -g read 13`
+        if [ $IGNITION_GPIO -ne 0 ]; then
+            sudo shutdown -h now
+        fi
+    elif [ $SET_SINK -ne 1 ]; then
+        SET_SINK=1
+        FIRST_INPUT=$(runuser -l pi -c "pacmd list-sink-inputs" | awk 'BEGIN { ORS=" " } /index:/ {printf "\r\n%s ", $2;} /state:/ {print $2} /channel map:/ {print $3} /application.process.binary =/ {print $3};' | grep "autoapp" | grep "mono"  | awk '{ print $1 }' | sed -n '1p')
+        SECOND_INPUT=$(runuser -l pi -c "pacmd list-sink-inputs" | awk 'BEGIN { ORS=" " } /index:/ {printf "\r\n%s ", $2;} /state:/ {print $2} /channel map:/ {print $3} /application.process.binary =/ {print $3};' | grep "autoapp" | grep "mono"  | awk '{ print $1 }' | sed -n '2p')
+        AA_MUSIC=$(runuser -l pi -c "pacmd list-sink-inputs" | awk 'BEGIN { ORS=" " } /index:/ {printf "\r\n%s ", $2;} /state:/ {print $2} /channel map:/ {print $3} /application.process.binary =/ {print $3};' | grep "autoapp" | grep "front"  | awk '{ print $1 }')
+        runuser -l pi -c "pacmd move-sink-input $FIRST_INPUT $SINK"
+        runuser -l pi -c "pacmd move-sink-input $SECOND_INPUT $SINK"
+        runuser -l pi -c "pacmd move-sink-input $AA_MUSIC Faded"
     fi
     # AUX redirect trigger
     AUX_GPIO=`gpio -g read 22`
@@ -56,11 +73,7 @@ do
         DAB_MOD=$(runuser -l pi -c "pactl load-module module-loopback source=$DAB sink=Faded")
     fi
     # Mute faded group when AA is talking, playing music or listening
-    AA_ASSISTANT=$(runuser -l pi -c "pacmd list-source-outputs" | awk 'BEGIN { ORS=" " } /index:/ {printf "/r/n%s ", $2;} /channel map:/ {print $3} /application.process.binary =/ {print $3};' | grep "mono" | grep "autoapp" | awk '{ print $1 }')
-    AA_MUSIC=$(runuser -l pi -c "pacmd list-sink-inputs" | awk 'BEGIN { ORS=" " } /index:/ {printf "\r\n%s ", $2;} /state:/ {print $2} /channel map:/ {print $3} /application.process.binary =/ {print $3};' | grep "autoapp" | grep "RUNNING" | grep "front" | awk '{ print $1 }')
-    AA_VOICE=$(runuser -l pi -c "pacmd list-sink-inputs" | awk 'BEGIN { ORS=" " } /index:/ {printf "\r\n%s ", $2;} /state:/ {print $2} /channel map:/ {print $3} /application.process.binary =/ {print $3};' | grep "autoapp" | grep "RUNNING" | grep "mono"  | awk '{ print $1 }')
-
-    if [ -z "$AA_ASSISTANT" ] && [ -z "$AA_VOICE" ] && [ -z "$AA_MUSIC" ] ; then
+    if [ -z "$AA_ASSISTANT" ] && [ -z "$AA_VOICE" ] ; then
         if [ $FADE_STATE -ne 0 ]; then
             FADE_STATE=0
             runuser -l pi -c "pactl set-sink-volume Faded 100%"
