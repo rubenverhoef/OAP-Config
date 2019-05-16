c1d66a252a5307b8819b03f2e832c285f9462a0e
Ruben Verhoef <ruben_verhoef@hotmail.com>
2019-05-16 18:17:51 +0200

    Fix #42 Open active session if already running

--- oap-config.sh
+++ oap-config.sh
@@ -115,6 +115,7 @@ function install_services() {
     install -m 755 /boot/OAP-Config/DAB/radio_cli                               "/opt/OAP/"
     install -m 755 /boot/OAP-Config/DAB/TuneDAB.sh                              "/opt/OAP/"
     install -m 755 /boot/OAP-Config/scripts/start_watchdog                      "/opt/OAP/"
+    install -m 755 /boot/OAP-Config/desktop/OAP_startup.sh                      "/opt/OAP/"
 }
 
 # Activate services
