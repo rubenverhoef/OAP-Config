0a5a69809d2455e5d24d7c65be20f44215a03a59
Ruben Verhoef <ruben_verhoef@hotmail.com>
2019-06-01 23:24:52 +0200

    #10 first version

---
+++ scripts/service_lightsensor.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python3 -u
+
+import smbus
+import os
+import RPi.GPIO as GPIO
+import subprocess
+from time import sleep
+
+# ---------------------------------
+# the addresss of TSL2561 can be
+# 0x29, 0x39 or 0x49
+BUS = 1
+TSL2561_ADDR = 0x39
+daynight_gpio = 0
+pwm_gpio = 12
+TSL2561_CHECK_INTERVAL=0.1
+LUX_DARK_BR=50
+LUX_FULL_BR=200
+LUX_DAY=100
+PWM_MAX=1023
+# ---------------------------------
+
+i2cBus = smbus.SMBus(BUS)
+
+# Start messure with 402 ms
+# (scale factor 1)
+i2cBus.write_byte_data(TSL2561_ADDR, 0x80, 0x03)
+
+lastvalue = 0
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(daynight_gpio, GPIO.OUT)
+os.system("gpio -g mode " + str(pwm_gpio) + " pwm")
+a=-(PWM_MAX/(LUX_FULL_BR-LUX_DARK_BR))
+b=(PWM_MAX+((PWM_MAX/(LUX_FULL_BR-LUX_DARK_BR)))*LUX_DARK_BR)
+
+try:
+  while True:
+    # read global brightness
+    # read low byte
+    LSB = i2cBus.read_byte_data(TSL2561_ADDR, 0x8C)
+    # read high byte
+    MSB = i2cBus.read_byte_data(TSL2561_ADDR, 0x8D)
+    Ambient = (MSB << 8) + LSB
+    #print ("Ambient: {}".format(Ambient))
+
+    # read infra red
+    # read low byte
+    LSB = i2cBus.read_byte_data(TSL2561_ADDR, 0x8E)
+    # read high byte
+    MSB = i2cBus.read_byte_data(TSL2561_ADDR, 0x8F)
+    Infrared = (MSB << 8) + LSB
+    #print ("Infrared: {}".format(Infrared))
+
+    # Calc visible spectrum
+    Visible = Ambient - Infrared
+    #print ("Visible: {}".format(Visible))
+
+    # Calc factor Infrared/Ambient
+    Ratio = 0
+    Lux = 0
+    step = 0
+    if Ambient != 0:
+      Ratio = float(Infrared)/float(Ambient)
+      #print ("Ratio: {}".format(Ratio))
+
+      # Calc lux based on data sheet TSL2561T
+      # T, FN, and CL Package
+      if 0 < Ratio <= 0.50:
+        Lux = 0.0304*float(Ambient) - 0.062*float(Ambient)*(Ratio**1.4)
+      elif 0.50 < Ratio <= 0.61:
+        Lux = 0.0224*float(Ambient) - 0.031*float(Infrared)
+      elif 0.61 < Ratio <= 0.80:
+        Lux = 0.0128*float(Ambient) - 0.0153*float(Infrared)
+      elif 0.80 < Ratio <= 1.3:
+        Lux = 0.00146*float(Ambient) - 0.00112*float(Infrared)
+      else:
+        Lux = 0
+      Luxrounded=round(Lux,0)
+      if lastvalue != Luxrounded:
+        #print ("Lux = {}\n".format(Luxrounded))
+        os.system("echo {} > /tmp/tsl2561".format(Luxrounded))
+        lastvalue = Luxrounded
+
+        Level=int(round(a*Luxrounded+b,0))
+        if Level < 0:
+          Level=0
+        elif Level > PWM_MAX:
+          Level=PWM_MAX
+
+        if Luxrounded > LUX_DAY:
+          GPIO.output(0, GPIO.LOW)
+        else:
+          GPIO.output(0, GPIO.HIGH)
+        os.system("gpio -g pwm " + str(pwm_gpio) + " " + str(Level))
+        print("Lux = {} | ".format(Luxrounded) + "Level = {}".format(Level))
+
+    sleep (TSL2561_CHECK_INTERVAL)
+except KeyboardInterrupt:
+  pass
+GPIO.cleanup()
